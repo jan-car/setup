7e03aa7e42a1ad8b3ba76231de595c5217c5124b
Jan Caron <jan.caron@outlook.de>
2024-05-18 17:37:06 +0200

    Personalize Setup

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,3 +2,10 @@ apt update
 apt upgrade -y
 
 apt install ansible aptitude -y
+
+# Afterwards:
+# ansible-playbook -K playbook.yml
+
+# Also install Meslo Nerd Font for the Powerlevel10k theme under Windows and set them for Windows Terminal,
+# they will be downloaded during playbook execution via dotfiles repo into "~/.local/share/fonts/NerdFonts"
+# More indo: https://github.com/romkatv/powerlevel10k#meslo-nerd-font-patched-for-powerlevel10k
