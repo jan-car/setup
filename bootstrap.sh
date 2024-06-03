1f4c11821bad303dec3babb80c4606db87b8c91c
Jan Caron <jan.caron@outlook.de>
2024-06-04 00:03:52 +0200

    Cleanup * Fix dotfiles install check * Bootstrap documentation * Remove superfluous asdf template * Fix poetry path directly after installation

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,9 +3,9 @@ apt upgrade -y
 
 apt install ansible aptitude -y
 
-# Afterwards:
+# Afterwards, execute in folder:
 # ansible-playbook -K playbook.yml
 
 # Also install Meslo Nerd Font for the Powerlevel10k theme under Windows and set them for Windows Terminal,
 # they will be downloaded during playbook execution via dotfiles repo into "~/.local/share/fonts/NerdFonts"
-# More indo: https://github.com/romkatv/powerlevel10k#meslo-nerd-font-patched-for-powerlevel10k
+# More info: https://github.com/romkatv/powerlevel10k#meslo-nerd-font-patched-for-powerlevel10k
